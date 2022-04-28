30d83f67da50b9e2ee8e9e1b7911359d3c6b70e3
Ann <cielitorepuyan@gmail.com>
2022-04-29 01:57:38 +0800

    updated
    changed dev

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-
+import Developers from './components/pages/Developers';
 import Header from './components/pages/Header';
 import Section from './components/pages/Section';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/pages/Developers.js
+++ src/components/pages/Developers.js
@@ -1,57 +1,57 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import '../styles/Developers.css'
-import Header from './Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { ClassNames } from "@emotion/react";
+import React,{useState} from "react";
+import '../styles/Developers.css';
 
 const Developers = () => {
-  return (
-    <div className='devbg'>
-    <React.Fragment>
-        <Header/>
-            <CssBaseline />
-            <Container id='devprofile'>
-                <Box sx={{ flexGrow: 1 }}>
-                    <h1 className='title'>Meet Our Developers</h1>
-                    <Grid container spacing={2} className='text-center' >
-                        <div className='contents'>
-                        <Grid item xs={4}>
-                            <div className='box slideup'>
-                                <img src='https://placeimg.com/640/480/people' className='img'/>
-                                <h2 id='name'>Meliza De Guia</h2>
-                                <p>Front-end Web Developer</p>
-                                <a href='#'><button className='btn'>See Portfolio</button></a>
-                            </div>
-                        </Grid>
 
-                        <Grid item xs={4}>
-                            <div className='box slideup'>
-                            <img src='https://placeimg.com/640/480/people' className='img'/>
-                                <h2 id='name'>Ann Repuyan</h2>
-                                <p>Front-end Web Developer</p>
-                                <a href='#'><button className='btn'>See Portfolio</button></a>
-                            </div>
-                        </Grid>
-
-                        <Grid item xs={4}>
-                            <div className='box slideup'>
-                            <img src='https://placeimg.com/640/480/people' className='img'/>
-                                <h2 id='name'>James Ricacho</h2>
-                                <p>Front-end Web Developer</p>
-                                <a href='#'><button className='btn'>See Portfolio</button></a>
-                            </div>
-                        </Grid>
+    const [name,setName] = useState('your name');
+    const [job,setJob] = useState('job title');
+    const [about,setAbout] = useState(' check this portfolio ');
+    return (
+        <>
+            <div className="d-flex gap-5" width="100px" height="100vh">
+                <div className='cards'>
+                    <div className='upper-container'>
+                        <div className="image-container">
+                            <img src="" alt='' height="100px" width="100px" />
                         </div>
-                    </Grid>
-                </Box>
-            </Container>
-        </React.Fragment>
-    </div>
-  )
+                    </div>
+                    <div className="lower-container">
+                        <h3> ( name ) </h3>
+                        <h4> ( job ) </h4>
+                        <p> (about) </p>
+                        <button> Visit Portfolio </button>
+                    </div>
+                </div>
+                <div className='cards'>
+                    <div className='upper-container'>
+                        <div className="image-container">
+                            <img src="" alt='' height="100px" width="100px" />
+                        </div>
+                    </div>
+                    <div className="lower-container">
+                        <h3> ( name ) </h3>
+                        <h4> ( job ) </h4>
+                        <p> (about) </p>
+                        <button> Visit Portfolio </button>
+                    </div>
+                </div>
+                <div className='cards'>
+                    <div className='upper-container'>
+                        <div className="image-container">
+                            <img src="" alt='' height="100px" width="100px" />
+                        </div>
+                    </div>
+                    <div className="lower-container">
+                        <h3> ( name ) </h3>
+                        <h4> ( job ) </h4>
+                        <p> (about) </p>
+                        <button> Visit Portfolio </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+    
 }
-
 export default Developers
